38d766f14e1c8db8d3461666792e06d29c72bdf0
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-05-28 19:35:12 +0300

    added ResourceServerCongig, Additional ClaimAccesTokenConverter to ResourceServer

---
+++ MyProject 01/SpringBoot_MyProject_01_OAuth2_JWT_Resource_Server/src/main/java/com/config/ResourceServerConfig.java
@@ -0,0 +1,49 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.auth.AdditionalClaimsAccessTokenConverter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Value("${publicKey}")
+    private String publicKey;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+    	 var converter = new AdditionalClaimsAccessTokenConverter();
+         converter.setVerifierKey(publicKey);
+         return converter;
+    }
+    
+    @Override
+	public void configure (HttpSecurity http) throws Exception {
+    	http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    	http.authorizeRequests().mvcMatchers("hello").hasAuthority("user")
+    		.and().authorizeRequests().anyRequest().authenticated();
+		http.cors().and();
+    }
+
+}
